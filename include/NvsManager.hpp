ab088b3cd00925f6adb31be38ea3bd7ce98fbc83
Leonardo Honda <leozinhohondams@gmail.com>
2025-09-22 11:14:41 -0400

    feat: add `NVS` to manage WiFi connection

---
+++ include/NvsManager.hpp
@@ -0,0 +1,21 @@
+#ifndef NVS_MANAGER_HPP
+#define NVS_MANAGER_HPP
+
+#include <Arduino.h>
+#include <Preferences.h>
+#include "nvs_flash.h"
+
+class NvsManager {
+public:
+  NvsManager();
+  bool LerNvsWifi(String &ssid, String &key);
+  bool GravarNvsWifi(const String &ssid, const String &key);
+  void LimparNamespace();
+
+private:
+  void inicializarNVS();  // Inicializa NVS e limpa se necessário
+  bool testarNamespace(); // Verifica se o namespace está acessível
+  const char* NAMESPACE_WIFI = "wifi";
+};
+
+#endif
