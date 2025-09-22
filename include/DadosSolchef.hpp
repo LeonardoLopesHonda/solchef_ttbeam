d88fa0e97c622ff218ebc94980c6b685054458c8
Leonardo Honda <leozinhohondams@gmail.com>
2025-09-22 13:30:08 -0400

    feat: implement `DadosSolchef` to store device data

---
+++ include/DadosSolchef.hpp
@@ -0,0 +1,19 @@
+#ifndef DADOSSOLCHEF_HPP
+#define DADOSSOLCHEF_HPP
+
+#include <Arduino.h>
+#include <ArduinoJson.h>
+
+class DadosSolchef {
+  public:
+    float tempAgua = 0;
+    float tempInterna = 0;
+    float latitude = 0;
+    float longitude = 0;
+    String horaRegistro;
+    String macAddressSender;
+    String macAddressReceiver;
+    String ToJSON() const;
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "Display.hpp"
 #include "WifiManager.hpp"
 #include "NvsManager.hpp"
+#include "DadosSolchef.hpp"
 
 // Objetos globais
 Display oled(17, 18, 21, 128, 64); // pino SDA, pino SCL, OLED_RESET, largura, altura
@@ -43,8 +44,12 @@ void setup() {
 }
 
 void loop() {
+  DadosSolchef dados;
+
+  dados.macAddressSender = wifiManager.getMacAddress().c_str();
+
   oled.Clear();
-  oled.PrintLine(0, ("IP: 192.168.---.---"));
+  oled.PrintLine(0, ("IP: " + std::string(wifiManager.getIPAddress())).c_str());
   oled.PrintLine(1, ("Ag: " + String(i) + " Cº").c_str());
   oled.PrintLine(2, ("Date: dd/MM/yyyy"));
   oled.PrintLine(3, ("GPS: xx.xx | xx.xx"));
